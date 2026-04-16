648485445f8e5a684b91498ff0c0fb183b1167c8
awm2006 <alexanderwmcmurray@gmail.com>
2026-04-16 19:18:08 -0400

    Update Wordle.java

--- Wordle.java
+++ Wordle.java
@@ -1,1 +1,11 @@
+import java.io.*;
+import java.util.*;
+
 public class Wordle{}
+
+    public static void main(String[] args) throws IOException {
+        List<String> words = loadWords("words.txt");
+        if (words.isEmpty()) {
+            System.out.println("Error: No valid words found in words.txt");
+            return;
+        }
