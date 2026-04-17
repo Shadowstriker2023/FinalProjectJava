2122fc35b6e6f283fb5e91a70ff7eea05bebc87b
awm2006 <alexanderwmcmurray@gmail.com>
2026-04-16 21:18:59 -0400

    Update Wordle.java

--- Wordle.java
+++ Wordle.java
@@ -5,7 +5,20 @@ public class Wordle{}
 
     public static void main(String[] args) throws IOException {
         List<String> words = loadWords("words.txt");
-        if (words.isEmpty()) {
+if (words.isEmpty()) {
             System.out.println("Error: No valid words found in words.txt");
             return;
         }
+
+        Random random = new Random();
+        String target = words.get(random.nextInt(words.size()));
+        Scanner scanner = new Scanner(System.in);
+        int maxGuesses = 6;
+        boolean won = false;
+
+        System.out.println("=== WORDLE ===");
+        System.out.println("Guess the 5-letter word. You have " + maxGuesses + " attempts.");
+        System.out.println("X = correct letter & position");
+        System.out.println("* = correct letter, wrong position");
+        System.out.println("+ = letter not in word");
+        System.out.println();
